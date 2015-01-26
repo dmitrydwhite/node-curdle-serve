a761bb189a0e598b8a1e0fc67b30f73dd43896ff
Dmitry White <dmitry.white@nike.com>
2015-01-26 13:10:32 -0800

    Updating for POSTing data.

--- curdle.js
+++ curdle.js
@@ -4,41 +4,23 @@
 	// File Requirements
 	var curl = require('curlrequest');
 
+	// Data Hash
+	var postData = {
+		// Enter data to post here
+	};
+
 	// Options Hash
 	var options = {
-		url: 'www.google.com',
-		include: true
+		url: 'http://rubusmap.herokuapp.com/api/patches',
+		method: 'POST',
+		include: true,
+		data: postData || false
 	};
-	var stdout, data, head, headers;
-	var headObj = {};
 
 	curl.request(options, function(err, output) {
 		if (err) throw err;
 
-		stdout = output.split('<');
-		data = stdout.pop();
-		head = stdout[0];
-		console.log('HEAD: ' + head);
-		headers = head.split('\n');
-
-		headers.forEach(function(line) {
-			var splitline;
-			if (line.indexOf(':') === -1) {
-				splitline = line.split(' ');
-				headObj[splitline[0]] = splitline[1] + ' ' + splitline[2];
-			} else {
-				splitline = line.split(':');
-				if (!headObj[splitline[0]]) {
-					headObj[splitline[0].trim()] = splitline[1].trim();
-				} else {
-					splitline[0] = splitline[0].trim();
-					splitline[0] += '+';
-					headObj[splitline[0].trim()] = splitline[1].trim();
-				}
-			}
-		});
-
-		console.log(headObj['Set-Cookie']);
+		console.log(output);
 	});
 
 })();
